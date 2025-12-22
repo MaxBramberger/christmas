2a086838e6e4d250ca6d52cd328a898ee757f93e
Max Bramberger <brambmax@yahoo.de>
2025-12-22 18:05:30 +0100

    add qr code component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {DisplayMessageComponent} from "./components/message-display.component";
+import {QrCodeComponent} from "./components/qr-code.component";
 
 export const routes: Routes = [
-    { path: 'message', component: DisplayMessageComponent}
+    { path: 'message', component: DisplayMessageComponent},
+    { path: 'qr', component: QrCodeComponent}
 ];
---
+++ src/app/components/qr-code.component.ts
@@ -0,0 +1,35 @@
+import {Component, inject, Input, OnChanges, OnInit} from '@angular/core';
+import {NgIf} from "@angular/common";
+import {QRCodeModule} from "angularx-qrcode";
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+    selector: 'app-qr-code',
+    template: `
+        @if(qrCodeData){
+            <qrcode [qrdata]="qrCodeData" [width]="256" [errorCorrectionLevel]="'M'"></qrcode>
+        } @else{
+            Invalid url.
+        }
+    `,
+    standalone: true,
+    imports: [
+        NgIf,
+        QRCodeModule
+    ]
+})
+export class QrCodeComponent implements OnInit {
+    qrCodeData: string | null = null;
+    route = inject(ActivatedRoute);
+
+    async ngOnInit() {
+        try {
+            const hashHex = this.route.snapshot.queryParamMap.get('key');
+            this.qrCodeData = `${window.location.origin}/message?key=${hashHex}`;
+            console.log(this.qrCodeData, this.route.root);
+        } catch (err) {
+            console.error('Failed to generate QR code', err);
+            this.qrCodeData = "";
+        }
+    }
+}
